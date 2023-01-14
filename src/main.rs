ddd5866cd1c869a7bdafecb3ae21947f4f9e603d
Vasile Gorcinschi <vasigorc@gmail.com>
2023-01-14 17:22:42 -0500

    :recycle: Added a Guess model

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+use guessing_game::model::Guess;
 
 fn main() {
     println!("Guess the number!");
@@ -19,14 +20,14 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
     
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
+
+        println!("You guessed: {}.", guess.value());
     
-        println!("You guessed: {guess}");
-    
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
